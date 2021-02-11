d4f2c4d419bae1fbe5a3689d9ed16d52bf3537be
kali666-6 <wael.jebri22@gmail.com>
2021-02-11 22:14:33 +0100

    fix

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
  * mul - checks if c is digit between 0 and 9 or not
- * a: first integer
- * b: second integer
+ * @a: first integer
+ * @b: second integer
  * Return: n  the multiplication value
  */
 int mul(int a, int b)
