72e51090c731aead5b42309a638bb8252eaea3b1
kali666-6 <wael.jebri22@gmail.com>
2021-02-28 21:29:07 +0100

    argc.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *main - print number of arguments 
+ *@argc : argc
+ *@argv : argv 
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n" argc - 1);
+return (0);
+}
