ad20d30bddb8a520961df6c2c5d09420504e3158
kali-linux <kali-linux@kali>
2021-02-28 21:09:01 +0100

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - print the name of the programme
+ *@argc: argc
+ *@argv: argv
+ *RETURN : 0
+ */
+int main ( int argc, char* argv[])
+{
+printf("%d\n", argc);
+return 0;
+}
