8719f1e94b43b29d9fb29d1cf8568872c66e002e
kali666-6 <wael.jebri22@gmail.com>
2021-03-10 23:27:52 +0100

    Fonction pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,10 @@
  * print_name - prints a name
  * @name: name to print
  * @f: function to print with
- *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && f != NULL)
+if (name != 0 && f != 0)
 f(name);
 }
