22b5cf1e01eaf0d8545652cb2756c26b54105569
root <root@kali-linux>
2021-05-01 20:01:37 +0200

    Doubly-Linked-L

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *print_dlistint - print list
+ *@h: list
+ *Return: number elements.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+int i = 0;
+
+if (h == NULL)
+{
+return (0);
+}
+for (h != NULL, h = h->next, i++)
+{
+printf("%d\n", h->n);
+}
+return (i);
+}
